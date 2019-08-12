b48a2d88de64553852380aa811b62fc8acf35bc3
manivannan <manivannan.pandiyan17@gmail.com>
2019-08-13 01:26:58 +1000

    Update TestRunner.java

--- src/test/java/com/nsw/testSuite/TestRunner.java
+++ src/test/java/com/nsw/testSuite/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/main/java/com/nsw/testCases/Scenarios.Feature", glue= {"com.nsw.testPages"}, plugin = { "pretty", "html:src/main/java/com/nsw/reports/cucumber-reports" }, monochrome=true)
+@CucumberOptions(features="src/test/java/com/nsw/testCases/Scenarios.Feature", glue= {"com.nsw.testPages"}, plugin = { "pretty", "html:src/test/java/com/nsw/reports/cucumber-reports" }, monochrome=true)
 public class TestRunner {
 
 }
